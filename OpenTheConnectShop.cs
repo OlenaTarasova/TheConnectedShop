f77dd37efaa1bbb1490ae9942acbada620ed080f
Elena Tarasova <tarasova.e.pe@gmail.com>
2025-11-03 15:51:14 +0200

    Add tests for checking Search field

--- OpenTheConnectShop.cs
+++ OpenTheConnectShop.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Playwright;
 
 namespace TheConnectShop
@@ -8,9 +9,9 @@ namespace TheConnectShop
     public class OpenTheConnectShop
     {
 
-    private IPlaywright _playwright;
-    private IBrowser _browser;
-    private IPage _page;
+        private IPlaywright _playwright;
+        private IBrowser _browser;
+        private IPage _page;
         private const string BaseUrl = "https://theconnectedshop.com/";
         [SetUp]
         public async Task Setup()
@@ -70,20 +71,32 @@ namespace TheConnectShop
         public async Task CheckPhoneNumber()
         {
             await _page.GotoAsync(BaseUrl);
-            var PhoneNumberValue = _page.Locator("a[href='tel:(305) 330-3424']"); //пошук якщо номер телефону не змінний
-            Assert.That(PhoneNumberValue, Is.Not.Null, "Phone number not found on the page");
+            var phoneNumberValue = _page.Locator("a[href='tel:(305) 330-3424']"); //пошук якщо номер телефону не змінний
+            Assert.That(await phoneNumberValue.CountAsync(), Is.GreaterThan(0), "Phone number not found on the page");
             /* 2 результаты за тегом document.querySelectorAll('a .header__customer-support-region__global-info');
 NodeList(2) [span.header__customer-support-region__global-info, span.header__customer-support-region__global-info] */
             var phoneNumberLocator = _page.Locator("a.header__customer-support-region__button", new() { HasText = "(305) 330-3424" });
             Assert.That(phoneNumberLocator, Is.Not.Null, "Phone number is not found on the page");
+            // /var phoneNumberLocator = _page.Locator("span.header__customer-support-region__global-info >> span.font-body-bold");
 
+
+            //     await Expect(phoneNumberLocator).ToBeVisibleAsync();
+
+
+            //     var phoneText = await phoneNumberLocator.InnerTextAsync();
+            //     Console.WriteLine($"Phone number text: {phoneText}");
+
+
+            //     Assert.That(phoneText.Trim()(прибирає всі додаткові символи), Is.EqualTo("(305) 330-3424"), "Phone number text does not match expected value");
         }
+
         [Test] //Профіль ( кнопка)
         public async Task CheckProfileButton()
         {
             await _page.GotoAsync(BaseUrl);
-            var ProfilyButtonValue = _page.Locator("svg.icon.icon-account").First;
-            Assert.That(ProfilyButtonValue, Is.Not.Null, "Profile value is not found on the page");
+            var profileButtonValue = _page.Locator("svg.icon.icon-account").First;
+            // Assert.That(ProfileButtonValue, Is.Not.Null, "Profile value is not found on the page");
+            Assert.That(await profileButtonValue.CountAsync(), Is.GreaterThan(0)); //цей варыант кращий
 
         }
 
@@ -93,8 +106,86 @@ NodeList(2) [span.header__customer-support-region__global-info, span.header__cus
             await _page.GotoAsync(BaseUrl);
             var CartValue = _page.Locator("#cart-icon-bubble");
             Assert.That(CartValue, Is.Not.Null, "Cart value is not found on the page");
-            
+
         }
 
-                       }
+        [Test] //корзина клік
+        public async Task CheckCartClick()
+        {
+            await _page.GotoAsync(BaseUrl);
+            var CartValue = _page.Locator("#cart-icon-bubble").ClickAsync;
+            Assert.That(CartValue, Is.Not.Null, "Cart value is not found on the page");
+
+        }
+
+        [Test]
+        public async Task CheckSearchFullNameProduct()
+        {
+            await _page.GotoAsync(BaseUrl);
+            var searchvalue = _page.Locator("#Search-In-Inline");
+            var placeholderSearch = await searchvalue.GetAttributeAsync("placeholder");
+            Assert.That(placeholderSearch, Is.EqualTo("Search"), $"error:{placeholderSearch}");
+            await searchvalue.FillAsync("ADA Smart Door Lock");// просимо заповнити
+            var inputSearchValue = await searchvalue.InputValueAsync(); //витягуємо значення
+            Assert.That(inputSearchValue, Is.EqualTo("ADA Smart Door Lock"), "ADA Smart Door Lock product not found"); // порівнюємо 
+            var searchButton = _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Search" });// чи можна просто через .Click();? як краще?
+            await searchButton.ClickAsync();
+            await _page.WaitForSelectorAsync("text=ADA Smart Door Lock");// чекаэм на результати пошуку
+            var resultFirstSearch = _page.Locator("a[href*='/products/ada-smart-door-lock']");// шукаєм карту продукту
+            Assert.That(await resultFirstSearch.IsVisibleAsync(), Is.True, "Product not found in search results");
+            await resultFirstSearch.ClickAsync(); //треба чи ні? хз
+
+        }
+        [Test]
+        public async Task CheckSearchOnePartOfNAme()//не повна назва та нижній регістр
+
+        {
+            await _page.GotoAsync(BaseUrl);
+            var searchvalue = _page.Locator("#Search-In-Inline");
+            var placeholderSearch = await searchvalue.GetAttributeAsync("placeholder");
+            Assert.That(placeholderSearch, Is.EqualTo("Search"), $"error:{placeholderSearch}");
+            await searchvalue.FillAsync("ada smart");
+            var inputSearchValue = await searchvalue.InputValueAsync();
+            Assert.That(inputSearchValue, Is.EqualTo("ada smart"), "ada smart not found");
+            var searchButton = _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Search" });
+            await searchButton.ClickAsync();
+            await _page.WaitForSelectorAsync("text=ada smart");// чекаэм на результати пошуку
+            var resultFirstSearch = _page.Locator("a[href*='/products/ada-smart-door-lock']");// шукаєм карту продукту
+            Assert.That(await resultFirstSearch.IsVisibleAsync(), Is.True, "Product not found in search results");
+            await resultFirstSearch.ClickAsync();
+        }
+
+        [Test] //негативний
+        public async Task Search_WithEmptyFild()
+        {
+            await _page.GotoAsync(BaseUrl);
+            var searchvalue = _page.Locator("#Search-In-Inline");
+            var placeholderSearch = await searchvalue.GetAttributeAsync("placeholder");
+            Assert.That(placeholderSearch, Is.EqualTo("Search"), $"error:{placeholderSearch}");
+            await searchvalue.FillAsync("");
+            var searchButton = _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Search" });
+            await searchButton.ClickAsync();
+            var inputValue = await searchvalue.InputValueAsync();// яку перевірку поставити?
+            Assert.That(inputValue, Is.EqualTo(""), "Search input should remain empty");
+
+        }
+        [Test]//перевірка неіснуючого товару
+        public async Task Search_non_ExistentProduct_AlertMessage()
+        {
+            await _page.GotoAsync(BaseUrl);
+            var searchvalue = _page.Locator("#Search-In-Inline");
+            var placeholderSearch = await searchvalue.GetAttributeAsync("placeholder");
+            Assert.That(placeholderSearch, Is.EqualTo("Search"), $"error:{placeholderSearch}");
+            string nonExistingProduct = "maincraft";//щоб можна було добавити різні слова для перевірки і перевірити введено слово в повідомлені
+            await searchvalue.FillAsync(nonExistingProduct);
+            var searchButton = _page.GetByRole(AriaRole.Button, new PageGetByRoleOptions { Name = "Search" });
+            await searchButton.ClickAsync();
+            await _page.WaitForSelectorAsync("text=maincraft");
+            var alertMessage = _page.Locator("p[role='status'].alert--warning");
+            await alertMessage.WaitForAsync(new LocatorWaitForOptions { State = WaitForSelectorState.Visible });//чекаємо поки появиться повідомлення
+            var alertMessageText = await alertMessage.InnerTextAsync();
+            Assert.That(alertMessageText, Does.Contain($"No results found for {nonExistingProduct}. Check the spelling or use a different word or phrase"), " No results message not displayed or incorrect");
+        }
+    }
 }
+

76d9bc63e9a9971671980e530be596c0c782f9af
Elena Tarasova <tarasova.e.pe@gmail.com>
2025-11-28 19:16:39 +0200

    SerchTest

--- Pages/SearchCons.cs
+++ Pages/SearchCons.cs
@@ -1,7 +1,7 @@
 public static class SearchTestConstants
 {
     public const string VALID_SEARCH_QUERY = "ADA Smart Door Lock";
-    public const string INVALID_SEARCH_QUERY = "invalid_product_query";
+    public const string INVALID_SEARCH_QUERY = "maincraft";
     public const string EMPTY_SEARCH_QUERY = "";
     public const string SPECIAL_CHARS_SEARCH_QUERY = "product@123";
 }
--- Pages/SearchPage.cs
+++ Pages/SearchPage.cs
@@ -12,25 +12,74 @@ namespace TheConnectedShop.Pages
         public SearchPage(IPage page) : base(page)
         {
             _searchField = Page.Locator("#Search-In-Inline");
-            _searchButton = Page.GetByRole(AriaRole.Button, new() { Name = "Search" });
+           // _searchButton = Page.GetByRole(AriaRole.Button, new() { Name = "Search" });
+          //  _searchButton = Page.Locator("button[type='submit'][aria-label='Search']");
+          _searchButton = Page.Locator("button[onclick*='Search-In-Inline']");
+          
+
         }
 
         public async Task Verify_Search_VAlid_ProductAsync(string VALID_SEARCH_QUERY)
         {
             await _searchField.FillAsync(VALID_SEARCH_QUERY);
+            await _searchButton.ClickAsync();
+            var result = Page.Locator("a[href*='/products/ada-smart-door-lock']");
+
+            await result.First.WaitForAsync(new() { State = WaitForSelectorState.Visible });
+
+            Assert.That(await result.First.IsVisibleAsync(), Is.True, "Product not found in search results");
                       
                    }
+
         public async Task Verify_Search_Invalid_ProductAsync(string INVALID_SEARCH_QUERY)
         {
             await _searchField.FillAsync(INVALID_SEARCH_QUERY);
+            await _searchButton.ClickAsync();
+            var alertMessage = Page.Locator(".alert--warning[role='status']");
+
+            await alertMessage.WaitForAsync(new() { State = WaitForSelectorState.Visible });
+ 
+            var alertText = await alertMessage.InnerTextAsync();
+
+            Console.WriteLine($"Alert message: {alertText}");
+ 
+           Assert.Multiple(() =>
+{
+    Assert.That(alertText, Does.Contain("No results found for").IgnoreCase,
+        "Не знайдено фрагмент 'No results found for'");
+
+    Assert.That(alertText, Does.Contain(INVALID_SEARCH_QUERY).IgnoreCase,
+        $"Не знайдено значення пошукового запиту: {INVALID_SEARCH_QUERY}");
+});
             }
         public async Task Verify_Search_Empty_ProductAsync(string EMPTY_SEARCH_QUERY)
         {
             await _searchField.FillAsync(EMPTY_SEARCH_QUERY);
+            await _searchButton.ClickAsync();
+            var inputValue = await _searchField.InputValueAsync();
+
+            Assert.That(inputValue, Is.EqualTo(""), "Search input should remain empty");
         }
         public async Task Verify_Search_Special_Chars_ProductAsync(string SPECIAL_CHARS_SEARCH_QUERY)
         {
             await _searchField.FillAsync(SPECIAL_CHARS_SEARCH_QUERY);
+            await _searchButton.ClickAsync();
+             var alertMessage = Page.Locator("p.alert--warning[role='status']");
+
+     await alertMessage.WaitForAsync(new() { State = WaitForSelectorState.Visible });
+ 
+            var alertText = await alertMessage.InnerTextAsync();
+
+            Console.WriteLine($"Alert message: {alertText}");
+ 
+               Assert.Multiple(() =>
+{
+    Assert.That(alertText, Does.Contain("No results found for").IgnoreCase,
+        "Не знайдено фрагмент 'No results found for'");
+
+    Assert.That(alertText, Does.Contain(SPECIAL_CHARS_SEARCH_QUERY).IgnoreCase,
+        $"Не знайдено значення пошукового запиту: {SPECIAL_CHARS_SEARCH_QUERY}");
+});
         }
     }
     }
